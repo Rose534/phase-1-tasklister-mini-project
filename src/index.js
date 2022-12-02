3792a90b6519a2fbac3d31984e540531c7347ff8
rose534 <wambuirose534@gmail.com>
2022-12-02 17:21:29 +0300

    list

--- src/index.js
+++ src/index.js
@@ -1,3 +1,34 @@
+const todo = 'create-task-form';
+
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  
+  const todoform = document.getElementById(todo);
+  todoform.addEventListener('submit', plus);
 });
+
+function plus(e) {
+  e.preventDefault();
+
+  const text = e.target.task.value;
+
+  const list = document.getElementById('tasks');
+
+  const another = document.createElement('li');
+  another.innerText = text;
+
+  const deletebtn = document.createElement('button');
+  deletebtn.innerText = ' x ';
+  another.append(deletebtn);
+
+  deletebtn.addEventListener('click', e => {
+    const li = e.target.parentNode;
+    li.remove();
+  })
+
+  list.append(another);
+
+  e.target.reset();
+
+
+console.log(text);
+}
